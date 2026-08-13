a39b0dce29b8c34fde22fcee1f8298a3c87c3754
Ayushi <ayushi.rajak11@gmail.com>
2026-08-13 21:43:31 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Skills from "./components/Skills";
 import Projects from "./components/Projects";
 import Education from "./components/Education";
 import Contact from "./components/Contact";
-import Footer from "./components/Footer";  
+import Footer from "./components/Footer";   
 
 function App() {
   return (
